6d592c6abc6aeeeb0e8cc11d20892e5746dfe8f4
Alexander Kell <Alexander.Kell@rapiddweller.com>
2021-05-25 07:22:22 +0200

    Revert "DBSeqHiLoGenerator constructor hard coded maxLo"

--- src/main/java/com/rapiddweller/platform/db/DBSeqHiLoGenerator.java
+++ src/main/java/com/rapiddweller/platform/db/DBSeqHiLoGenerator.java
@@ -45,7 +45,7 @@ public class DBSeqHiLoGenerator extends HiLoGenerator {
    * @param maxLo the max lo
    */
   public DBSeqHiLoGenerator(String name, int maxLo) {
-	this(name, maxLo, null);
+    this(name, 100, null);
   }
 
   /**
