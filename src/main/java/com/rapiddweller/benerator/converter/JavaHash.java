f3827ba9c266e5c1a775a1986364eba85a9fc3d9
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-09-13 20:34:17 +0200

    Created JavaHash as a more efficient hash Converter than the cryptographic ones

---
+++ src/main/java/com/rapiddweller/benerator/converter/JavaHash.java
@@ -0,0 +1,44 @@
+/* (c) Copyright 2021 by Volker Bergmann. All rights reserved. */
+
+package com.rapiddweller.benerator.converter;
+
+import com.rapiddweller.common.ConversionException;
+import com.rapiddweller.common.Converter;
+import com.rapiddweller.common.ThreadAware;
+
+/**
+ * Converts an object into a hex representation of its hash code.<br/><br/>
+ * Created: 13.09.2021 15:43:54
+ * @author Volker Bergmann
+ * @since 1.2.0
+ */
+public class JavaHash implements Converter<Object,String>, ThreadAware {
+
+  @Override
+  public boolean isThreadSafe() {
+    return true;
+  }
+
+  @Override
+  public boolean isParallelizable() {
+    return true;
+  }
+
+  @Override
+  public Class<Object> getSourceType() {
+    return Object.class;
+  }
+
+  @Override
+  public Class<String> getTargetType() {
+    return String.class;
+  }
+
+  @Override
+  public String convert(Object sourceValue) throws ConversionException {
+    String sourceText = (sourceValue != null ? sourceValue.toString() : "");
+    int hashCode = sourceText.hashCode();
+    return Integer.toHexString(hashCode);
+  }
+
+}
