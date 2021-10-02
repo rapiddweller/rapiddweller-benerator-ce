8ef7e0803ec3cafed545c3bb6220a7d158d23a24
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-10-02 11:59:23 +0200

    Improved robustness and test coverage of DbUnitEntityExporter and

--- src/main/java/com/rapiddweller/platform/dbunit/DbUnitEntityExporter.java
+++ src/main/java/com/rapiddweller/platform/dbunit/DbUnitEntityExporter.java
@@ -45,13 +45,11 @@ import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.sax.TransformerHandler;
 import javax.xml.transform.stream.StreamResult;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Map;
 
 /**
- * Exports entities in DbUnit XML file format.
- *
+ * Exports entities in DbUnit XML file format.<br/><br/>
  * @author Volker Bergmann
  * @since 0.3.04
  */
@@ -72,74 +70,42 @@ public class DbUnitEntityExporter extends AbstractConsumer {
   private String uri;
   private String encoding;
 
+  private State state;
   private OutputStream out;
   private TransformerHandler handler;
 
 
   // constructors ----------------------------------------------------------------------------------------------------
 
-  /**
-   * Instantiates a new Db unit entity exporter.
-   */
   public DbUnitEntityExporter() {
     this(DEFAULT_URI);
   }
 
-  /**
-   * Instantiates a new Db unit entity exporter.
-   *
-   * @param uri the uri
-   */
   public DbUnitEntityExporter(String uri) {
     this(uri, DEFAULT_FILE_ENCODING);
   }
 
-  /**
-   * Instantiates a new Db unit entity exporter.
-   *
-   * @param uri      the uri
-   * @param encoding the encoding
-   */
   public DbUnitEntityExporter(String uri, String encoding) {
     setUri(uri);
     setEncoding(encoding);
     this.toStringConverter = new ToStringConverter(null, DATE_PATTERN, TIMESTAMP_PATTERN);
+    this.state = State.CREATED;
   }
 
   // properties ------------------------------------------------------------------------------------------------------
 
-  /**
-   * Gets uri.
-   *
-   * @return the uri
-   */
   public String getUri() {
     return uri;
   }
 
-  /**
-   * Sets uri.
-   *
-   * @param uri the uri
-   */
   public void setUri(String uri) {
     this.uri = uri;
   }
 
-  /**
-   * Gets encoding.
-   *
-   * @return the encoding
-   */
   public String getEncoding() {
     return encoding;
   }
 
-  /**
-   * Sets encoding.
-   *
-   * @param encoding the encoding
-   */
   public void setEncoding(String encoding) {
     this.encoding = (encoding != null ? encoding : SystemInfo.getFileEncoding());
     if (this.encoding == null) {
@@ -156,12 +122,8 @@ public class DbUnitEntityExporter extends AbstractConsumer {
     }
     Entity entity = (Entity) object;
     try {
-      if (logger.isDebugEnabled()) {
-        logger.debug("exporting " + entity);
-      }
-      if (out == null) {
-        initPrinter();
-      }
+      logger.debug("exporting {}", entity);
+      initializeIfCreated();
       AttributesImpl atts = new AttributesImpl();
       for (Map.Entry<String, Object> entry : entity.getComponents().entrySet()) {
         Object value = entry.getValue();
@@ -175,8 +137,6 @@ public class DbUnitEntityExporter extends AbstractConsumer {
       }
       handler.startElement("", "", entity.type(), atts);
       handler.endElement("", "", entity.type());
-    } catch (IOException e) {
-      throw new RuntimeException(e);
     } catch (SAXException e) {
       throw new ConfigurationError("Error in processing element: " + entity, e);
     }
@@ -191,6 +151,10 @@ public class DbUnitEntityExporter extends AbstractConsumer {
 
   @Override
   public void close() {
+    initializeIfCreated();
+    if (state == State.CLOSED) {
+      return;
+    }
     if (handler != null) {
       try {
         handler.endElement("", "", "dataset");
@@ -205,9 +169,15 @@ public class DbUnitEntityExporter extends AbstractConsumer {
     }
   }
 
-  //  java.lang.String overrides --------------------------------------------------------------------------------------
+  // private helpers -------------------------------------------------------------------------------------------------
 
-  private void initPrinter() throws IOException {
+  private void initializeIfCreated() {
+    if (state == State.CREATED) {
+      initialize();
+    }
+  }
+
+  private void initialize() {
     try {
       // create file and write header
       SAXTransformerFactory tf = (SAXTransformerFactory) SAXTransformerFactory.newInstance();
@@ -219,18 +189,33 @@ public class DbUnitEntityExporter extends AbstractConsumer {
 
       out = new FileOutputStream(uri);
       handler.setResult(new StreamResult(out));
+
+      // bug fix: the following extra call to start/endDocument fixes the bug,
+      // that in normal invocation no line separator is inserted after the <?xml?> header
+      handler.startDocument();
+      handler.endDocument();
+      // end of bug fix
+
       handler.startDocument();
       handler.startElement("", "", "dataset", null);
 
+      this.state = State.INITIALIZED;
     } catch (TransformerConfigurationException e) {
       throw new ConfigurationError(e);
-    } catch (SAXException e) {
+    } catch (Exception e) {
       throw new ConfigurationError("Error in initializing XML file", e);
     }
   }
+
+  private enum State {
+    CREATED, INITIALIZED, CLOSED
+  }
+
+  //  java.lang.String overrides --------------------------------------------------------------------------------------
 
   @Override
   public String toString() {
     return getClass().getSimpleName() + '[' + uri + ", " + encoding + "]";
   }
+
 }
