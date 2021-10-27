bc2f25207921080a25fbb36ea22e97cdc906f6de
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-10-27 14:28:54 +0200

    Created 'Append' converter

---
+++ src/main/java/com/rapiddweller/benerator/converter/Append.java
@@ -0,0 +1,48 @@
+/* (c) Copyright 2021 by Volker Bergmann. All rights reserved. */
+
+package com.rapiddweller.benerator.converter;
+
+import com.rapiddweller.common.ConversionException;
+import com.rapiddweller.common.converter.AbstractConverter;
+import com.rapiddweller.common.converter.ToStringConverter;
+
+/**
+ * {@link com.rapiddweller.common.Converter} implementation which creates a String representation
+ * of the object and appends a suffix.<br/><br/>
+ * Created: 27.10.2021 11:42:49
+ * @author Volker Bergmann
+ * @since 2.1.0
+ */
+public class Append extends AbstractConverter<Object, String> {
+
+  private String suffix;
+
+  public Append() {
+    this("_");
+  }
+
+  public Append(String suffix) {
+    super(Object.class, String.class);
+    this.suffix = suffix;
+  }
+
+  @Override
+  public String convert(Object sourceValue) throws ConversionException {
+    if (sourceValue == null) {
+      return null;
+    }
+    String string = ToStringConverter.convert(sourceValue, null);
+    return string + suffix;
+  }
+
+  @Override
+  public boolean isThreadSafe() {
+    return true;
+  }
+
+  @Override
+  public boolean isParallelizable() {
+    return true;
+  }
+
+}
---
+++ src/test/java/com/rapiddweller/benerator/converter/AppendTest.java
@@ -0,0 +1,54 @@
+/* (c) Copyright 2021 by Volker Bergmann. All rights reserved. */
+
+package com.rapiddweller.benerator.converter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Tests the {@link Append} converter.<br/><br/>
+ * Created: 27.10.2021 12:01:15
+ * @author Volker Bergmann
+ * @since 2.1.0
+ */
+public class AppendTest {
+
+  @Test
+  public void testNormal() {
+    Append a = new Append();
+    assertEquals("123_", a.convert("123"));
+    assertEquals("__", a.convert("_"));
+  }
+
+  @Test
+  public void testEmpty() {
+    Append a = new Append();
+    assertEquals("_", a.convert(""));
+  }
+
+  @Test
+  public void testNull() {
+    Append a = new Append();
+    assertNull(a.convert(null));
+  }
+
+  @Test
+  public void test_suffix() {
+    Append a = new Append("_demo");
+    assertEquals("123_demo", a.convert("123"));
+    assertEquals("_demo", a.convert(""));
+    assertNull(a.convert(null));
+  }
+
+  @Test
+  public void test_non_string_source() {
+    Append a = new Append("_demo");
+    assertEquals("123_demo", a.convert(123));
+    assertEquals("-1_demo", a.convert(-1));
+    assertEquals("true_demo", a.convert(true));
+    assertEquals("A_demo", a.convert('A'));
+  }
+
+}
