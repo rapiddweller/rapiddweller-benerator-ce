44fdba4764009cf17e07d0d9c351d410596f51b2
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-11-04 20:23:45 +0100

    Testing JavaHash

---
+++ src/test/java/com/rapiddweller/benerator/converter/JavaHashTest.java
@@ -0,0 +1,25 @@
+/* (c) Copyright 2021 by Volker Bergmann. All rights reserved. */
+
+package com.rapiddweller.benerator.converter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests the {@link JavaHash} class.<br/><br/>
+ * Created: 04.11.2021 20:16:41
+ * @author Volker Bergmann
+ * @since 2.1.0
+ */
+public class JavaHashTest {
+
+  @Test
+  public void test() {
+    JavaHash hash = new JavaHash();
+    assertEquals("00000000", hash.convert(null));
+    assertEquals("00000000", hash.convert(""));
+    assertEquals("0027b8b2", hash.convert("Test"));
+  }
+
+}
