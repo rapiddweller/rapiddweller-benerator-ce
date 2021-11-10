206aa66f7305be9ed173807c74bb124fbdfb3709
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-11-10 11:26:07 +0100

    Testing UUID of Postgres

---
+++ src/test/java/com/rapiddweller/platform/db/postgres/PostgresUUIDTest.java
@@ -0,0 +1,21 @@
+/* (c) Copyright 2021 by Volker Bergmann. All rights reserved. */
+
+package com.rapiddweller.platform.db.postgres;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Tests Postgres access.<br/><br/>
+ * Created: 07.11.2021 17:12:55
+ * @author Volker Bergmann
+ * @since 2.1.0
+ */
+public class PostgresUUIDTest extends AbstractProstgresIntegrationTest {
+
+  @Test @Ignore("So far there is no agreed way to set this up uniformly on CI and a local system")
+  public void testUUID() {
+    assertMinGenerations(100, () -> parseAndExecuteFile(folder + "/postgres-uuid.ben.xml"));
+  }
+
+}
