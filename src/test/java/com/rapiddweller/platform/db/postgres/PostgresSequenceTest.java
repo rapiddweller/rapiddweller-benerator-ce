7358ad6425d2fb725d8f0f595d14945b0a650a3c
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-11-10 11:26:58 +0100

    Testing sequences on Postgres

---
+++ src/test/java/com/rapiddweller/platform/db/postgres/PostgresSequenceTest.java
@@ -0,0 +1,21 @@
+/* (c) Copyright 2021 by Volker Bergmann. All rights reserved. */
+
+package com.rapiddweller.platform.db.postgres;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Tests sequence support for Postgres.<br/><br/>
+ * Created: 09.11.2021 10:15:05
+ * @author Volker Bergmann
+ * @since 2.1.0
+ */
+public class PostgresSequenceTest extends AbstractProstgresIntegrationTest {
+
+  @Test //@Ignore("So far there is no agreed way to set this up uniformly on CI and a local system")
+  public void testSequence() {
+    assertMinGenerations(100, () -> parseAndExecuteFile(folder + "/postgres-seq.ben.xml"));
+  }
+
+}
