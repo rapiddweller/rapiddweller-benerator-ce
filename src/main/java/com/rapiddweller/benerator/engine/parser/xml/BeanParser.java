e4516dec5e94ca7d78d32046e9428df1408f6620
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-12-10 20:31:04 +0100

    Improved <bean> parser

--- src/main/java/com/rapiddweller/benerator/engine/parser/xml/BeanParser.java
+++ src/main/java/com/rapiddweller/benerator/engine/parser/xml/BeanParser.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2006-2020 by rapiddweller GmbH & Volker Bergmann. All rights reserved.
+ * (c) Copyright 2006-2021 by rapiddweller GmbH & Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -30,15 +30,18 @@ import com.rapiddweller.benerator.BeneratorErrorIds;
 import com.rapiddweller.benerator.engine.BeneratorContext;
 import com.rapiddweller.benerator.engine.BeneratorRootStatement;
 import com.rapiddweller.benerator.engine.Statement;
+import com.rapiddweller.benerator.engine.parser.string.IdParser;
 import com.rapiddweller.benerator.engine.statement.BeanStatement;
 import com.rapiddweller.benerator.engine.statement.IfStatement;
 import com.rapiddweller.benerator.factory.BeneratorExceptionFactory;
 import com.rapiddweller.common.ConversionException;
+import com.rapiddweller.common.Validator;
 import com.rapiddweller.common.exception.ExceptionFactory;
 import com.rapiddweller.common.exception.ParseException;
-import com.rapiddweller.common.StringUtil;
+import com.rapiddweller.common.parser.FullyQualifiedClassNameParser;
 import com.rapiddweller.common.xml.XMLUtil;
 import com.rapiddweller.format.xml.AttrInfoSupport;
+import com.rapiddweller.format.xml.AttributeInfo;
 import com.rapiddweller.script.Assignment;
 import com.rapiddweller.script.BeanSpec;
 import com.rapiddweller.script.DatabeneScriptParser;
@@ -51,8 +54,13 @@ import org.slf4j.Logger;
 import org.w3c.dom.Element;
 
 import static com.rapiddweller.benerator.engine.DescriptorConstants.ATT_CLASS;
+import static com.rapiddweller.benerator.engine.DescriptorConstants.ATT_DEFAULT;
 import static com.rapiddweller.benerator.engine.DescriptorConstants.ATT_ID;
+import static com.rapiddweller.benerator.engine.DescriptorConstants.ATT_NAME;
+import static com.rapiddweller.benerator.engine.DescriptorConstants.ATT_REF;
+import static com.rapiddweller.benerator.engine.DescriptorConstants.ATT_SOURCE;
 import static com.rapiddweller.benerator.engine.DescriptorConstants.ATT_SPEC;
+import static com.rapiddweller.benerator.engine.DescriptorConstants.ATT_VALUE;
 import static com.rapiddweller.benerator.engine.DescriptorConstants.EL_BEAN;
 import static com.rapiddweller.benerator.engine.DescriptorConstants.EL_PROPERTY;
 
@@ -64,35 +72,84 @@ import static com.rapiddweller.benerator.engine.DescriptorConstants.EL_PROPERTY;
  */
 public class BeanParser extends AbstractBeneratorDescriptorParser {
 
+  // <bean> format spec ----------------------------------------------------------------------------------------------
+
   // Named staticLogger to avoid name clash with logger from parent class
   private static final Logger staticLogger = LoggerFactory.getLogger(BeanParser.class);
 
-  private static final AttrInfoSupport ATTR_INFO;
-  static {
-    ATTR_INFO = new AttrInfoSupport(BeneratorErrorIds.SYN_BEAN_ILLEGAL_ATTR);
-    ATTR_INFO.add(ATT_ID, true, BeneratorErrorIds.SYN_BEAN_ID);
-    ATTR_INFO.add(ATT_CLASS, false, BeneratorErrorIds.SYN_BEAN_CLASS);
-    ATTR_INFO.add(ATT_SPEC, false, BeneratorErrorIds.SYN_BEAN_SPEC);
-  }
+  /** 'id' attribute info for &lt;bean&gt; elements */
+  private static final AttributeInfo<String> ID_REQ = new AttributeInfo<>(
+      ATT_ID, true, BeneratorErrorIds.SYN_BEAN_ID, new IdParser(), null);
+
+  /** 'id' attribute info for &lt;consumer&gt; and &lt;runtask&gt; elements and others */
+  private static final AttributeInfo<String> ID_OPT = new AttributeInfo<>(
+      ATT_ID, false, BeneratorErrorIds.SYN_BEAN_ID, new IdParser(), null);
+
+  private static final AttributeInfo<String> CLASS = new AttributeInfo<>(
+      ATT_CLASS, false, BeneratorErrorIds.SYN_BEAN_CLASS, new FullyQualifiedClassNameParser(), null
+  );
+
+  private static final AttributeInfo<String> SPEC = new AttributeInfo<>(
+      ATT_SPEC, false, BeneratorErrorIds.SYN_BEAN_SPEC, null, null);
+
+  private static final AttrInfoSupport BEAN_ATTR_INFO = new AttrInfoSupport(
+      BeneratorErrorIds.SYN_BEAN_ILLEGAL_ATTR, new BeanElementValidator(), ID_REQ, CLASS, SPEC);
+
+  // <property> format spec ------------------------------------------------------------------------------------------
+
+  private static final AttributeInfo<String> PROP_NAME = new AttributeInfo<>(
+      ATT_NAME, true, BeneratorErrorIds.SYN_BEAN_PROP_NAME, new IdParser(), null);
+
+  private static final AttributeInfo<String> PROP_VALUE = new AttributeInfo<>(
+      ATT_VALUE, false, BeneratorErrorIds.SYN_BEAN_PROP_VALUE, null, null);
+
+  private static final AttributeInfo<String> PROP_DEFAULT = new AttributeInfo<>(
+      ATT_DEFAULT, false, BeneratorErrorIds.SYN_BEAN_PROP_DEFAULT, null, null);
+
+  private static final AttributeInfo<String> PROP_REF = new AttributeInfo<>(
+      ATT_REF, false, BeneratorErrorIds.SYN_BEAN_PROP_REF, null, null);
+
+  private static final AttributeInfo<String> PROP_SOURCE = new AttributeInfo<>(
+      ATT_SOURCE, false, BeneratorErrorIds.SYN_BEAN_PROP_SOURCE, null, null);
+
+  private static final AttrInfoSupport PROP_ATTR_INFO = new AttrInfoSupport(
+      BeneratorErrorIds.SYN_BEAN_PROP_ELEMENT, new BeanPropertyValidator(),
+      PROP_NAME, PROP_VALUE, PROP_DEFAULT, PROP_REF, PROP_SOURCE);
+
+  // constructor & interface -----------------------------------------------------------------------------------------
 
   public BeanParser() {
     // only allowed in non-loop statements in order to avoid leaks
-    super(EL_BEAN, ATTR_INFO, BeneratorRootStatement.class, IfStatement.class);
+    super(EL_BEAN, BEAN_ATTR_INFO, BeneratorRootStatement.class, IfStatement.class);
   }
 
+  @Override
+  public BeanStatement doParse(Element element, Element[] parentXmlPath, Statement[] parentPath, BeneratorParseContext context) {
+    attrSupport.validate(element);
+    try {
+      String id = ID_REQ.parse(element);
+      Expression<?> bean = parseBeanExpression(element, true);
+      return new BeanStatement(id, bean, context.getResourceManager());
+    } catch (ConversionException e) {
+      throw BeneratorExceptionFactory.getInstance().configurationError("Error parsing bean element", e);
+    }
+  }
+
+  // public static utility methods -----------------------------------------------------------------------------------
+
   @SuppressWarnings({"unchecked", "rawtypes"})
-  public static Expression<?> parseBeanExpression(Element element) {
-    String id = element.getAttribute(ATT_ID);
-    Expression<?> instantiation = null;
-    String beanSpec = element.getAttribute(ATT_SPEC);
-    String beanClass = element.getAttribute(ATT_CLASS);
-    if (!StringUtil.isEmpty(beanSpec)) {
+  public static Expression<?> parseBeanExpression(Element element, boolean idRequired) {
+    String id = (idRequired ? ID_REQ : ID_OPT).parse(element);
+    String beanSpec = SPEC.parse(element);
+    String beanClass = CLASS.parse(element);
+    Expression<?> instantiation;
+    if (beanSpec != null) {
       try {
         instantiation = DatabeneScriptParser.parseBeanSpec(beanSpec);
       } catch (ParseException e) {
         throw BeneratorExceptionFactory.getInstance().configurationError("Error parsing bean spec: " + beanSpec, e);
       }
-    } else if (!StringUtil.isEmpty(beanClass)) {
+    } else if (beanClass != null) {
       staticLogger.debug("Instantiating bean of class {} (id={})", beanClass, id);
       instantiation = new DefaultConstruction(beanClass);
     } else {
@@ -104,21 +161,21 @@ public class BeanParser extends AbstractBeneratorDescriptorParser {
   }
 
   @SuppressWarnings({"unchecked", "rawtypes"})
-  public static BeanSpec resolveBeanExpression(Element element, BeneratorContext context) {
-    String id = element.getAttribute(ATT_ID);
+  public static BeanSpec resolveLocalBeanExpression(Element element, BeneratorContext context) {
+    String id = ID_OPT.parse(element);
+    String beanClass = CLASS.parse(element);
+    String beanSpec = SPEC.parse(element);
     Expression<?> instantiation;
-    String beanSpecString = element.getAttribute(ATT_SPEC);
-    String beanClass = element.getAttribute(ATT_CLASS);
     boolean ref = false;
-    if (!StringUtil.isEmpty(beanSpecString)) {
+    if (beanSpec != null) {
       try {
-        BeanSpec spec = DatabeneScriptParser.resolveBeanSpec(beanSpecString, context);
-        instantiation = ExpressionUtil.constant(spec.getBean());
-        ref = spec.isReference();
+        BeanSpec specObject = DatabeneScriptParser.resolveBeanSpec(beanSpec, context);
+        instantiation = ExpressionUtil.constant(specObject.getBean());
+        ref = specObject.isReference();
       } catch (ParseException e) {
-        throw BeneratorExceptionFactory.getInstance().configurationError("Error parsing bean spec: " + beanSpecString, e);
+        throw BeneratorExceptionFactory.getInstance().configurationError("Error parsing bean spec: " + beanSpec, e);
       }
-    } else if (!StringUtil.isEmpty(beanClass)) {
+    } else if (beanClass != null) {
       staticLogger.debug("Instantiating bean of class {} (id={})", beanClass, id);
       instantiation = new DefaultConstruction<>(beanClass);
     } else {
@@ -129,6 +186,7 @@ public class BeanParser extends AbstractBeneratorDescriptorParser {
       if (!EL_PROPERTY.equals(propertyElement.getNodeName())) {
         throw ExceptionFactory.getInstance().syntaxErrorForXmlElement("not a supported bean child element", propertyElement);
       }
+      PROP_ATTR_INFO.validate(propertyElement);
     }
     Assignment[] propertyInitializers = mapPropertyDefinitions(propertyElements);
     Object result = new BeanConstruction(instantiation, propertyInitializers).evaluate(context);
@@ -143,20 +201,29 @@ public class BeanParser extends AbstractBeneratorDescriptorParser {
     return assignments;
   }
 
+  // private helpers -------------------------------------------------------------------------------------------------
+
   private static Assignment parseProperty(Element propertyElement) {
-    String propertyName = propertyElement.getAttribute("name");
+    String propertyName = PROP_NAME.parse(propertyElement);
     Expression<?> value = SettingParser.parseValue(propertyElement);
     return new Assignment(propertyName, value);
   }
 
-  @Override
-  public BeanStatement doParse(Element element, Element[] parentXmlPath, Statement[] parentPath, BeneratorParseContext context) {
-    try {
-      String id = element.getAttribute(ATT_ID);
-      Expression<?> bean = parseBeanExpression(element);
-      return new BeanStatement(id, bean, context.getResourceManager());
-    } catch (ConversionException e) {
-      throw BeneratorExceptionFactory.getInstance().configurationError("Error parsing bean element", e);
+  static class BeanElementValidator implements Validator<Element> {
+    @Override
+    public boolean valid(Element element) {
+      assertAtLeastOneAttributeIsSet(element, ATT_CLASS, ATT_SPEC);
+      mutuallyExcludeAttributes(element, ATT_CLASS, ATT_SPEC);
+      return true;
+    }
+  }
+
+  static class BeanPropertyValidator implements Validator<Element> {
+    @Override
+    public boolean valid(Element element) {
+      assertAtLeastOneAttributeIsSet(element, ATT_VALUE, ATT_REF, ATT_DEFAULT, ATT_SOURCE);
+      mutuallyExcludeAttributes(element, ATT_VALUE, ATT_REF, ATT_DEFAULT, ATT_SOURCE);
+      return true;
     }
   }
 
