6c39c84255c1390db30421c4789226e6d44751b8
ake2l <Alexander.Kell@rapiddweller.com>
2022-07-28 11:17:15 +0200

    fix(memstore): make sumEntityColumn method more reliable with better logging and support numeric values in form of string

--- src/main/java/com/rapiddweller/platform/memstore/MemStore.java
+++ src/main/java/com/rapiddweller/platform/memstore/MemStore.java
@@ -157,6 +157,17 @@ public class MemStore extends AbstractStorageSystem {
           result += (Integer) value;
         } else if (value != null && value.getClass() == Double.class) {
           result += (Double) value;
+        }  else if (value != null && value.getClass() == String.class) {
+          try {
+            result += Integer.parseInt((String) value);
+          } catch (NumberFormatException e1) {
+            logger.warn("Could not parse {} as integer try to parse as double", value);
+            try {
+              result += Double.parseDouble((String) value);
+            } catch (NumberFormatException e2) {
+              logger.warn("Could not parse value {}, not a numeric value", value);
+            }
+          }
         } else {
           throw new IllegalArgumentError(
               String.format("Entity %s does not have a column with name %s or given column is not a numeric value", type, column));
