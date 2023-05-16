0de7d73c371dfd71dc3c23b4276068a5ba72b2ee
Dang Ly <lyhoangdang96@gmail.com>
2023-05-16 23:07:58 +0700

    Add integration test

--- src/test/java/com/rapiddweller/platform/db/mongodb/MongoDBSimpleIntegrationTest.java
+++ src/test/java/com/rapiddweller/platform/db/mongodb/MongoDBSimpleIntegrationTest.java
@@ -5,6 +5,8 @@ import com.rapiddweller.benerator.test.AbstractBeneratorIntegrationTest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThrows;
+
 public class MongoDBSimpleIntegrationTest extends AbstractBeneratorIntegrationTest {
     @Test
     public void mongoDBIdTest() {
@@ -18,5 +20,11 @@ public class MongoDBSimpleIntegrationTest extends AbstractBeneratorIntegrationTe
         assumeTestActive("mongodb");
         BeneratorContext context = parseAndExecuteFile("demo/db/mongodb-inserter.ben.xml");
         Assert.assertEquals("demo/db", context.getContextUri());
+    }
+
+    @Test
+    public void mongoDBInsertionRuntimeErrorInSameCollectionTest() {
+        assumeTestActive("mongodb");
+        assertThrows(RuntimeException.class, () -> parseAndExecuteFile("demo/db/mongodb-insert-into-same-collection.ben.xml"));
     }
 }
