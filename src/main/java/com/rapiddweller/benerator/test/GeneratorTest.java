666474f827e32faa03bcbe43c8be6666fb9cf0ba
Alexander Kell <Alexander.Kell@rapiddweller.com>
2023-10-12 09:57:10 +0700

    (test) increase relative weight tolerance by 5 %

--- src/main/java/com/rapiddweller/benerator/test/GeneratorTest.java
+++ src/main/java/com/rapiddweller/benerator/test/GeneratorTest.java
@@ -352,7 +352,7 @@ public abstract class GeneratorTest extends ModelTest {
         assertTrue("Generated set does not contain value " + value, productSet.contains(value));
         double measuredWeight = counter.getRelativeCount(value);
         assertTrue("For value '" + value + "', weight " + expectedWeight + " is expected, but it is " + measuredWeight,
-            Math.abs(measuredWeight - expectedWeight) / expectedWeight < 0.15);
+            Math.abs(measuredWeight - expectedWeight) / expectedWeight < 0.20);
       } else {
         assertFalse("Generated contains value " + value + " though it has zero weight", productSet.contains(value));
       }
